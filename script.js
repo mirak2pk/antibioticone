979ec66bdc68b347d56a81521cceed5e49c4dd91
Abdelkarim Achtaou <karime.parkour@gmail.com>
2024-07-08 19:47:00 +0100

    updates

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const galleryContainer = document.querySelector('.gallery-container');
 const galleryControlsContainer = document.querySelector('.gallery-controls');
 const galleryControls = ['previous','next'];
-const galleryItems = document.querySelectorAll('gallery-item');
+const galleryItems = document.querySelectorAll('.gallery-item');
 
 class Carousel {
     constructor(container, items, controls){
